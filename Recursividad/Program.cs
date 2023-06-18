c8fa4d159975389ba3fb97924242a9d5dc9e52df
DieguitoCraft <DieguitoCraft@DIEGUITOCRAFT>
2023-06-17 20:00:11 -0500

    Agregar archivos de proyecto.

---
+++ Recursividad/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+
+namespace Recursividad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("INGRESE EL NUMERO A SUMAR");
+            int valor = int.Parse(Console.ReadLine());
+            Console.WriteLine(suma(valor) + "\nGRACIAS POR USAR EL PROGRAMA :)");
+        }
+        public static int suma(int n)
+        {
+            if (n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return n + suma(n-1);
+            }
+        }
+    }
+}
